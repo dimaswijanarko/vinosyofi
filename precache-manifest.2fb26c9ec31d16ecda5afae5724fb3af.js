94057aaef0a9832939863588f371e7457db26041
Sukma Rangga Pradita Wijaya <46914976+sukmaranggapradeta@users.noreply.github.com>
2020-08-02 21:22:27 +0700

    Updates

---
+++ precache-manifest.2fb26c9ec31d16ecda5afae5724fb3af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a4662f429a847dd3b3d088d0c1515a",
+    "url": "/vinosyofi/index.html"
+  },
+  {
+    "revision": "6e8135239f911d62b4be",
+    "url": "/vinosyofi/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "aea5f87d0a1ad205de5d",
+    "url": "/vinosyofi/static/css/main.b62993c4.chunk.css"
+  },
+  {
+    "revision": "6e8135239f911d62b4be",
+    "url": "/vinosyofi/static/js/2.27cf1ba6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vinosyofi/static/js/2.27cf1ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea5f87d0a1ad205de5d",
+    "url": "/vinosyofi/static/js/main.62985b31.chunk.js"
+  },
+  {
+    "revision": "6dbe980cfeca86144c74",
+    "url": "/vinosyofi/static/js/runtime-main.35923a85.js"
+  },
+  {
+    "revision": "9551dc36a8defd7cb18f5733affdeed3",
+    "url": "/vinosyofi/static/media/love.9551dc36.png"
+  },
+  {
+    "revision": "db6a3ef23f619e189e156952a46e5873",
+    "url": "/vinosyofi/static/media/syofi.db6a3ef2.jpeg"
+  },
+  {
+    "revision": "a4fff28800fa42660ecd7d5b12d77246",
+    "url": "/vinosyofi/static/media/vino.a4fff288.jpeg"
+  }
+]);
